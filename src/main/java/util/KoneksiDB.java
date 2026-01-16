ac145b17ab38bdf9fa8ed8160bcbbce1ddce0d42
Samashi11 <maulashidqic10@gmail.com>
2026-01-16 17:44:12 +0700

    remove unwanted file

--- src/main/java/util/KoneksiDB.java
+++ src/main/java/util/KoneksiDB.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class KoneksiDB {
 
     private static final String URL =
-        "jdbc:postgresql://localhost:5432/db_baru";
+        "jdbc:postgresql://localhost:5432/keuangan_pribadi";
     private static final String USER = "postgres";
     private static final String PASS = "bismillah";
 
